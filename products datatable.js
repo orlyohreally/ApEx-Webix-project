9539623758e174b65e7095c0c8c38bf47e1f4deb
orlyohreally <orli-knop@ya.ru>
2018-07-01 10:26:13 +0300

    chart data defined by pl sql process

--- products datatable.js
+++ products datatable.js
@@ -9,8 +9,24 @@ function getDataTableGrid(container, data, columns) {
         columns: columns,
         data: data,
         on:{
-            onItemDblClick:function(){
-                $$("editwin").show();
+            onItemDblClick:function(id){
+                console.log(id);
+                console.log(this.getItem(id.row));
+                apex.server.process("GET_PRODUCT_LINE_CHART_DATA", 
+                {
+                    x01: this.getItem(id.row).id,
+                },
+                {
+                    dataType: "json",
+                    success: function(data) {
+                        console.log(data);
+                        $$("product_chart").parse(data.product_stats);
+                        $$("editwin").show();
+                    },
+                    error: function(res) {
+                        console.log(res);
+                    }
+                });
             }
         }
     });
@@ -85,9 +101,9 @@ webix.ready(function(){
                             },
                             {
                                 view:"chart",
-                                container:"chartDiv",
                                 type:"line",
-                                value:"#sales#",
+                                id: "product_chart",
+                                value:"#total#",
                                 height: 300,
                                 item:{
                                     borderColor: "#1293f8",
@@ -98,29 +114,18 @@ webix.ready(function(){
                                     width:3
                                 },
                                 xAxis:{
-                                    template:"'#year#"
+                                    template:"'#month_year#"
                                 },
                                 offset:0,
                                 yAxis:{
                                     start:0,
-                                    end:100,
+                                    end:20,
                                     step:10,
                                     template:function(obj){
                                         return (obj%20?"":obj)
                                     }
                                 },
-                                data: [
-                                    { id:1, sales:20, year:"02"},
-                                    { id:2, sales:55, year:"03"},
-                                    { id:3, sales:40, year:"04"},
-                                    { id:4, sales:78, year:"05"},
-                                    { id:5, sales:61, year:"06"},
-                                    { id:6, sales:35, year:"07"},
-                                    { id:7, sales:80, year:"08"},
-                                    { id:8, sales:50, year:"09"},
-                                    { id:9, sales:65, year:"10"},
-                                    { id:10, sales:59, year:"11"}
-                                ]
+                                data: []
                             }
                         ],
                         elementsConfig: {
@@ -145,6 +150,7 @@ webix.ready(function(){
 
 
 /*
+-------------------PROCESS GET_PRODUCTS--------------
 DECLARE
     l_cursor sys_refcursor;
 BEGIN
@@ -158,4 +164,23 @@ BEGIN
     htp.p(APEX_JSON.get_clob_output);
     APEX_JSON.free_output;
 END;
+
+-------------------PROCESS GET_PRODUCT_LINE_CHART_DATA--------------
+DECLARE
+    l_cursor sys_refcursor;
+BEGIN
+    OPEN l_cursor FOR
+    SELECT sum(quantity) "total", TO_CHAR(completed_at, 'MM/YYYY') "month_year"
+    FROM orders o LEFT JOIN order_product op ON o.id = op.order_id
+    WHERE o.status_id = 5 AND op.product_id = 1
+    GROUP BY TO_CHAR(completed_at, 'MM/YYYY');
+    APEX_JSON.initialize_clob_output;
+    APEX_JSON.open_object;
+    APEX_JSON.write('product_stats', l_cursor);
+    APEX_JSON.close_object;
+    htp.p(APEX_JSON.get_clob_output);
+    APEX_JSON.free_output;
+END;
+
+
 */
