664cf2b368fcdf2a9331d65a72501dfc10d4780d
orlyohreally <orli-knop@ya.ru>
2018-07-01 10:27:13 +0300

    updated orders table

--- orders.sql
+++ orders.sql
@@ -2,6 +2,7 @@ CREATE TABLE orders (
   id number(9) NOT NULL PRIMARY KEY,
   customer_id number(9) NOT NULL,
   status_id number(9) NOT NULL,
+  completed_at date,
   created_at date,
   created_by varchar2(50),
   updated_at date default sysdate,
@@ -11,6 +12,9 @@ CREATE TABLE orders (
 );
 
 CREATE SEQUENCE order_seq START WITH 1 INCREMENT BY 1;
+
+CREATE INDEX orders_customer_ind ON orders (customer_id);
+CREATE INDEX orders_status_ind ON orders (status_id);
 
 CREATE OR REPLACE TRIGGER order_bef_ins
 BEFORE INSERT
