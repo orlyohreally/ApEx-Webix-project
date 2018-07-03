6e74b16320e18f82681534cab912e3803081acb0
orlyohreally <orli-knop@ya.ru>
2018-07-04 00:22:38 +0300

    added getting of customer form html

--- customers - page dynamic actions.js
+++ customers - page dynamic actions.js
@@ -1,4 +1,18 @@
 //show customer information on click
-console.log($(this.triggeringElement), $(this.triggeringElement).find('.customer'), $(this.triggeringElement).find('.customer').attr('oid'));
-
 const id = $(this.triggeringElement).find('.customer').attr('oid');
+console.log('id', id, $v('pFlowId'),$v('pInstance'));
+$.ajax({
+      type: "GET",
+      url: "wwv_flow.show",
+      data: {
+        p_flow_id      : $v('pFlowId'),
+        p_flow_step_id:$('#pFlowStepId').val(), 
+        p_instance:$('#pInstance').val(), 
+    },
+     dataType: "html",
+    success: function(data){
+        console.log(data);
+        $($(data).find("#customer_form")).modal('show');
+console.log($($(data).find("#customer_form")).html());
+    }
+  });
