9e7cadedcbd94b6e4e183653e0c0d3be1e9d219b
orlyohreally <orli-knop@ya.ru>
2018-07-05 22:41:23 +0300

    opening up a dialog with customer data

--- customers - page dynamic actions.js
+++ customers - page dynamic actions.js
@@ -1,18 +1,30 @@
 //show customer information on click
 const id = $(this.triggeringElement).find('.customer').attr('oid');
-console.log('id', id, $v('pFlowId'),$v('pInstance'));
 $.ajax({
-      type: "GET",
-      url: "wwv_flow.show",
-      data: {
+    type: "GET",
+    url: "wwv_flow.show",
+    data: {
         p_flow_id      : $v('pFlowId'),
         p_flow_step_id:$('#pFlowStepId').val(), 
         p_instance:$('#pInstance').val(), 
+        p_arg_names: ['P3_ACTION', 'P3_CUSTOMER_ID'],
+        p_arg_values: ['edit_customer', id]
     },
      dataType: "html",
     success: function(data){
-        console.log(data);
-        $($(data).find("#customer_form")).modal('show');
-console.log($($(data).find("#customer_form")).html());
+        const html = $(data).find("#modal_hldr").html();
+        $("#modal_hldr").html(html);
+        $("#customer_form").dialog();
     }
-  });
+});
+/*
+IF :P3_CUSTOMER_ID IS NOT NULL THEN
+    SELECT last_name, first_name,  middle_name into :P3_CUSTOMER_LASTNAME, :P3_CUSTOMER_FIRSTNAME, :P3_CUSTOMER_MIDDLENAME
+    FROM customers
+    WHERE id = :P3_CUSTOMER_ID;
+ELSE
+    :P3_CUSTOMER_LASTNAME := NULL;
+    :P3_CUSTOMER_FIRSTNAME := NULL;
+    :P3_CUSTOMER_MIDDLENAME := NULL;
+END IF;
+*/
